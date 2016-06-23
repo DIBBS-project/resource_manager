b33f87e0a495f449b9545c497782d7e66c3c48cb
Maverick Chardet <chardetm@gmail.com>
2016-06-23 16:13:49 -0500

    Updating the API client for the new Appliance Registry models

--- rpapp/ar_client/apis/__init__.py
+++ rpapp/ar_client/apis/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 # import apis into api package
-from .events_api import EventsApi
+from .actions_api import ActionsApi
+from .appliances_api import AppliancesApi
 from .scripts_api import ScriptsApi
-from .softwares_api import SoftwaresApi
 from .users_api import UsersApi
